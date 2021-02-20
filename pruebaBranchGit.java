c101d4779e91158f584debf6a5b0f283020ebbba
khandaniyal <daniyal.khan@iesjoandaustria.org>
2021-02-20 02:40:38 +0000

    int x added from master branch

--- pruebaBranchGit.java
+++ pruebaBranchGit.java
@@ -1,6 +1,6 @@
 public class pruebaBranchGit {
 	public static void main(String[] args) {
 	
-	int x = 1; //añadido desde el branch de developer1	
+	int x = 1; //añadido desde el branch de master 	
  }
 }
