a6353f7ce0d742451a46c81c726b68038f36cb1e
khandaniyal <daniyal.khan@iesjoandaustria.org>
2021-02-25 20:47:04 +0100

    amplicaion

--- 03_04_elpenjat/danifunciones.java
+++ 03_04_elpenjat/danifunciones.java
@@ -3,12 +3,12 @@ import java.util.List;
 import java.io.*;
 public class danifunciones {
 	
-	public static boolean paraulaRandomActual(String randParaules, List<Character> characterInput){
+	public static boolean paraulaRandomActual(String par, List<Character> characterInput){
 	System.out.print("Paraula: ");
 	int contadorCorrectes = 0;
-	for(int i=0; i < randParaules.length(); i++){
-		if(characterInput.contains(randParaules.charAt(i))){
-			System.out.print(randParaules.charAt(i));
+	for(int i=0; i < par.length(); i++){
+		if(characterInput.contains(par.charAt(i))){
+			System.out.print(par.charAt(i));
 			contadorCorrectes++;
 		}
 		else{
@@ -16,15 +16,15 @@ public class danifunciones {
 		}
 	}
 	System.out.println();	
-	return (randParaules.length() == contadorCorrectes); //devuelve true si acierta todos lo chars de la palabra
+	return (par.length() == contadorCorrectes); //devuelve true si acierta todos lo chars de la palabra
 	}
 
-	public static boolean adivinaJugador(BufferedReader entrada, String randParaules, List<Character> characterInput) throws IOException{
+	public static boolean adivinaJugador(BufferedReader entrada, String par, List<Character> characterInput) throws IOException{
 		
 		System.out.println("Introdueix una lletra");
         	String adivinaLletra = entrada.readLine();  //input de las letras que vas a adivinar
         	characterInput.add(adivinaLletra.charAt(0)); //añade la primera letra del string al arraylist de chars
-
+		
 		if("prou".equals(adivinaLletra)){ //aún pensar lógica 
 			
 			return true;
@@ -32,15 +32,26 @@ public class danifunciones {
 		return false;
    	}
 
-/*	public static void utilitzades(String adivinaLletra, String randParaules, List<Character> characterInput)throws IOException{
+	public static void utilitzades(String par, List<Character> characterInput)throws IOException{
 		
-		for(int i=0; i < randParaules.length(); i++){
-			if(adivinaLletra == randParaules.charAt(i)){
-				System.out.println("Utilitzades: " + randParaules.charAt(i));
-			}
-		}
-	}
-*/	
+
+	}	
 
 }
+/*for(int i = 0; i < args.length; i++){
 
+                if(i == args.length-2){
+                        System.out.print(args[i] + " i ");
+                }
+
+                else if(i == args.length-1){
+                        System.out.println(args[i]);
+                }
+                else{
+                        System.out.print(args[i] + ", ");
+                }
+        }
+
+        System.out.println();
+    }
+*/
